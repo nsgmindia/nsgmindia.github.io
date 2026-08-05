7ca24dd19299020f085efc2e6f3a8a051c709b47
nsgmindia <nsgmindia@gmail.com>
2026-08-05 16:38:02 +0530

    Add files via upload

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -8,7 +8,12 @@ $("#copy").click(function(){
 // $("#print").click(function() { window.print(); });
 $("#print").click(function(){
 	ele = $("#dashboard")[0];
-	options = { margin: 5, filename: 'Smart Metering Statistics.pdf', image: { type: 'jpeg', quality: 0.98 }, html2canvas: { scale: 2, useCORS: true, scrollY: 0, width: 1600, windowWidth: 1600 }, jsPDF: { unit: 'pt', format: 'a4', orientation: 'portrait' } };
+	options = {
+		margin: 5, filename: 'Smart Metering Statistics.pdf',
+		image: { type: 'jpeg', quality: 0.98 },
+		html2canvas: { scale: 2, useCORS: true, scrollY: 0, width: 1600, windowWidth: 1600, onclone: function(clonedDoc) { const clonedEle = $("#dashboard"); clonedEle.style.width = '1600px', clonedEle.style.maxWidth = 'none', clonedEle.style.display = 'flex', clonedEle.style.flexWrap = 'nowrap' },
+		jsPDF: { unit: 'mm', format: 'a3', orientation: 'portrait' }
+	};
 	html2pdf().set(options).from(ele).save();
 });
 function openDiv(evt, divid) {
