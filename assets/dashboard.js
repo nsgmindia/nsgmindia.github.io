b9c020e558669d58a0ca19afff426db5cac6d90a
nsgmindia <nsgmindia@gmail.com>
2026-09-11 13:23:43 +0530

    Add files via upload

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -184,7 +184,7 @@ $(document).ready(function() {
 		$('.fe').on('click', function() { $('#tableFeeder').DataTable().buttons(0).trigger(); });
 		$('.fp').on('click', function() { $('#tableFeeder').DataTable().buttons(1).trigger(); });
 	});
-	$.getJSON("https://api.counterapi.dev/v2/nsgmindia/nsgmindia/up").done(function(response) {const apiCount=response.data?.up_count||0;const hits=1100+apiCount; $('#views').text(hits.);}).fail(function() {$('#views').text(1100);});
+	$.getJSON("https://api.counterapi.dev/v2/nsgmindia/nsgmindia/up").done(function(response) {const apiCount=response.data?.up_count||0;const hits=1100+apiCount; $('#views').text(hits);}).fail(function() {$('#views').text(1100);});
 	$(document).ready(function() {
 		$(window).scroll(function() { if($(window).scrollTop()>100) {$('#backToTop').fadeIn(300)} else {$('#backToTop').fadeOut(300)} });
 		$('#backToTop').click(function() {window.scrollTo({ top: 0, behavior: 'smooth' });});
